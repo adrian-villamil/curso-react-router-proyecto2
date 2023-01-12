7e03ec4cad0c83adf19a0116d7f2a9712bf07207
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-01-12 01:32:14 -0500

    Le enviamos el texto de un TODO al TodoForm en la propiedad defaultTodoText, llegando desde el useLocation() o mediante el uso de la función getTodo().

--- src/routes/edit/EditTodoPage.js
+++ src/routes/edit/EditTodoPage.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { TodoForm } from "../../ui/TodoForm";
 import { useTodos } from "../useTodos";
 
 function EditTodoPage() {
   const params = useParams();
   const id = Number(params.id);
-  const { stateUpdaters } = useTodos();
+  const location = useLocation();
+
+  const { state, stateUpdaters } = useTodos();
+  const { loading, getTodo } = state;
   const { editTodo } = stateUpdaters;
 
+  let todoText;
+
+  if (location.state?.todoText) {
+    todoText = location.state.todoText;
+  } else if (loading) {
+    return <p>Cargando...</p>
+  } else {
+    const todo = getTodo(id);
+    todoText = todo.text;
+  }
   return (
     <TodoForm
       label='Edita tu TODO'
+      defaultTodoText={todoText}
       submitText='Editar'
       submitEvent={(newText) => editTodo(id, newText)}
     />
