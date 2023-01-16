e88d3d5690440d0a43f060fa336199c4bd7984cf
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-01-16 17:55:43 -0500

    Recibimos por props el estado y el actualizador de useSearchParams. Cada vez que el usuario escribe algo en el buscador, ese string será reflejado en el query search de la url. Además se podrá filtrar los todos si se modifica el query search en la url desde otra ventana o pestaña del navegador.

--- src/ui/TodoSearch/index.js
+++ src/ui/TodoSearch/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './TodoSearch.css';
 
-function TodoSearch({ searchValue, setSearchValue, loading }) {
+function TodoSearch({ searchValue, setSearchValue, searchParams, setSearchParams, loading }) {
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
     setSearchValue(event.target.value);
+    let params;
+    if (event.target.value) {
+      params = {
+        search: event.target.value,
+      }
+    } else {
+      params = undefined;
+    }
+    setSearchParams(params);
   };
+
+  useEffect(() => {
+    const search = searchParams.get('search') ?? '';
+    setSearchValue(search);
+  }, [searchParams]);
 
   return (
     <input
